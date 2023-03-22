ec159996795c7e4a119b4e8f71cec4987097fce5
KyoHolic <adrianmkoch@hotmail.com>
2023-03-22 17:17:14 +0100

    added AddTourButton and DeleteTourButton and their Actions

--- src/main/java/at/fhtw/swen2/tutorial/presentation/view/NewPersonView.java
+++ src/main/java/at/fhtw/swen2/tutorial/presentation/view/NewPersonView.java
@@ -1,18 +1,27 @@
 package at.fhtw.swen2.tutorial.presentation.view;
 
+import at.fhtw.swen2.tutorial.presentation.ViewManager;
 import at.fhtw.swen2.tutorial.presentation.viewmodel.NewPersonViewModel;
 import at.fhtw.swen2.tutorial.service.PersonService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 @Component
@@ -28,9 +37,16 @@ public class NewPersonView implements Initializable {
     private NewPersonViewModel newPersonViewModel;
 
     @FXML
+    private Button addTourButton;
+
+    @FXML
+    private Button deleteTourButton;
+
+    @FXML
     private Text feedbackText;
     @FXML
     private TextField nameTextField;
+
 
     @Override
     public void initialize(URL location, ResourceBundle rb) {
@@ -42,7 +58,23 @@ public class NewPersonView implements Initializable {
             feedbackText.setText("nothing entered!");
             return;
         }
-
         newPersonViewModel.addNewPerson();
+    }
+
+    public void addTourButtonAction(ActionEvent event) {
+        feedbackText.setText("Add Tour Button pressed!");
+        /*try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../resources/at/fhtw/swen2/tutorial/presentation/view/NewPerson.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public void deleteTourButtonAction(ActionEvent event) {
+        feedbackText.setText("Delete Tour Button pressed!");
     }
 }
